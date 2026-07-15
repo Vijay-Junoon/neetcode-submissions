6e83b4104cd55a3d6b555f509c589124a26e2f43
V Vijay <85287539+Vijay-Junoon@users.noreply.github.com>
2026-07-15 10:25:01 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        hash = dict()
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            if sorted_word in hash:
+                hash[sorted_word].append(word)
+            else:
+                hash[sorted_word] = [word]
+            
+        return list(hash.values())
