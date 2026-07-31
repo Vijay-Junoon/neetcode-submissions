7e3ec9cdf2a26ad040adfc53a0bde473d698c64d
V Vijay <85287539+Vijay-Junoon@users.noreply.github.com>
2026-07-31 07:27:59 +0530

    Add: car-fleet - submission-4

---
+++ Data Structures & Algorithms/car-fleet/submission-4.py
@@ -0,0 +1,19 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        
+        arr = []
+        for i in range(len(position)):
+            distance = target - position[i]
+            time = distance / speed[i]
+            arr.append((position[i],time))
+        
+        arr.sort(key = lambda x: x[0],reverse = True)
+        
+        fleets = 0
+        mTime = 0
+        for p,t in arr:
+            if t > mTime:
+                mTime = t
+                fleets += 1
+        
+        return fleets
